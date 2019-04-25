7d945e3ca96ab20651a819efe54e3f0151e1b717
mark <markopondo.m@gmail.com>
2019-04-25 14:36:32 +0300

    deployment

---
+++ src/app/strikethrough.directive.ts
@@ -0,0 +1,25 @@
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
+
+@Directive({
+  selector: '[appStrikethrough]'
+})
+
+export class StrikethroughDirective {
+
+  constructor(private elem: ElementRef) {}
+
+  @HostListener('click') onclick() {
+    this.textDeco('line-through');
+  }
+
+
+  @HostListener('dblclick') onDoubleClick() {
+    this.textDeco('None');
+  }
+
+  private textDeco(action: string) {
+    this.elem.nativeElement.style.textDecoration = action;
+
+   }
+}
